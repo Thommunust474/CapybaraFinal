c054d282951e74f16741258e88770618217fec04
Thommunust474 <132861878+Thommunust474@users.noreply.github.com>
2023-08-07 14:17:07 +1200

    adding if statements

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -4,20 +4,18 @@
   let snoutY = headY - 8; // link to snout location Y
   let eyeY = headY - 15;
 
-
   let headsize = 83;
   let snoutsize = 36;
   let earsize = 85;
   let eyesize = 90; 
   let nostrilSize = 8;
 
-  
+  angry = false;
+
   // Nostril - snout
   let leftNostrilX = headX - snoutsize / 2.8;
   let rightNostrilX = headX + snoutsize / 2.8;
   let nostrilY = snoutY + snoutsize / 3.2;
-
-
 
 function setup_wallpaper(pWallpaper) {
   pWallpaper.output_mode(GLIDE_WALLPAPER);
@@ -31,18 +29,32 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(120, 190, 190); //light honeydew green colour
+  if (angry == true){
+    background(187, 63, 63)
+  }
+  else{
+    background(120, 190, 190); //light honeydew green colour
+  }
 }
 
 function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
 
   // Draw ears
-  fill(128, 100, 100);
+  if (angry == true){
+    fill(160, 80, 60)
+  }
+  else{
+    fill(128, 100, 100);
+  }
   ellipse(headX - 25, headY - 35, earsize / 4, earsize / 3.2); // Left ear
   ellipse(headX + 25, headY - 35, earsize / 4, earsize / 3.2); // Right ear
 
   // Draw capybara head
-  fill(160, 120, 90); // Light brown
+  if (angry == true){
+    fill(160, 60, 60)
+  }
+  else{  fill(160, 120, 90); // Light brown
+}
   stroke(20); // Dark brown
   ellipse(headX, headY, headsize, headsize / 0.84);
 
@@ -53,7 +65,12 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   ellipse(headX + 34, eyeY, eyesize / 8.2, eyesize / 7.5); // Right eye
 
   // Eyelids
-  fill(120, 100, 75); // Light brown eyelids
+  if (angry == true){
+    fill(100, 40, 40)
+  }
+  else{
+    fill(120, 100, 75); // Light brown eyelids
+  }
   stroke(40);
   let cornereyelidRadius = headsize / 30;
   
@@ -62,7 +79,12 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
 
   // Draw snout
   stroke(30);
-  fill(128, 100, 90);
+  if (angry == true){
+    fill(160, 80, 60)
+  }
+  else{
+    fill(128, 100, 90);
+  }
   var cornerRadius = 16;
   rect(headX - snoutsize / 2, snoutY, snoutsize, snoutsize * 1.6, cornerRadius);
 
@@ -91,4 +113,18 @@ function my_symbol() { // do not rename this function. Treat this similarly to a
   line(headX + 25, headY + 15, headX + 45, headY + 15);
   line(headX + 25, headY + 20, headX + 45, headY + 25);
   line(headX + 25, headY + 25, headX + 45, headY + 35);
+
+  // angry eyebrows
+  stroke(40);
+  if (angry == true){
+    strokeWeight(2)
+    line(headX - 40, headY -30, headX - 25, headY - 20)
+    line(headX + 40, headY -30, headX + 25, headY - 20)
+    strokeWeight(1)
+  }
+  
+  else{
+    line(headX - 25, headY + 15, headX - 45, headY + 15)
+  }
+   
 }
